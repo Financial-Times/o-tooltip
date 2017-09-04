da640667a5afa96cddf1e594d03b9abacdae22da
Alex Florisca <alexflorisca@gmail.com>
2017-09-04 12:34:12 +0100

    Added unit tests for Target

--- src/js/target.js
+++ src/js/target.js
@@ -1,12 +1,6 @@
 class Target {
 	constructor(targetEl) {
 		this.targetEl = targetEl;
-		this.rectObject = targetEl.getBoundingClientRect();
-	}
-
-	getEdge(edge){
-		const edges = {"top": this.top, "bottom": this.bottom, "right": this.right, "left": this.left};
-		return edges[edge];
 	}
 
 	get offsetTop() {
