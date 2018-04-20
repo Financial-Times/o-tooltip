fec25a4105f08298d93bb19676d0799c5fef80fe
notlee <lee.moody@ft.com>
2018-04-20 18:22:17 +0100

    Restore non-drawing tests.

--- test/Tooltip.test.js
+++ test/Tooltip.test.js
@@ -2,6 +2,7 @@
 import proclaim from 'proclaim';
 import sinon from 'sinon/pkg/sinon';
 import * as fixtures from './helpers/fixtures';
+import createMockRaf from 'mock-raf';
 import Tooltip from './../main';
 import Viewport from 'o-viewport';
 
@@ -246,7 +247,140 @@ describe("Tooltip", () => {
 		});
 	});
 
-	describe.only('drawTooltip', () => {
+	describe("show", () => {
+		let checkOptionsStub;
+		let getOptionsStub;
+		let drawTooltipStub;
+		let resizeListenerStub;
+		let targetStub;
+		let closeStub;
+
+		beforeEach(() => {
+			getOptionsStub = sinon.stub(Tooltip, 'getOptions').returns({});
+			checkOptionsStub = sinon.stub(Tooltip, 'checkOptions').returnsArg(0);
+			drawTooltipStub = sinon.stub(Tooltip.prototype, 'drawTooltip');
+			resizeListenerStub = sinon.stub(Tooltip.prototype, 'resizeListener');
+			closeStub = sinon.stub(Tooltip.prototype, 'close');
+
+			targetStub = sinon.stub(Tooltip, "Target");
+
+			fixtures.declarativeCode();
+		});
+
+		afterEach(() => {
+			getOptionsStub.restore();
+			checkOptionsStub.restore();
+			drawTooltipStub.restore();
+			resizeListenerStub.restore();
+			closeStub.restore();
+			targetStub.restore();
+
+			fixtures.reset();
+		});
+
+
+		it('sets up a close handler for touch on the tooltip-close button', () => {
+			const tooltipEl = document.getElementById('tooltip-demo');
+			const testTooltip = new Tooltip(tooltipEl, { target: 'demo-tooltip-target' });
+			testTooltip.render();
+
+			const tooltipCloseEl = tooltipEl.querySelector('.o-tooltip-close');
+
+			const e = new Event('touchend');
+			tooltipCloseEl.dispatchEvent(e);
+
+			proclaim.isFalse(closeStub.called);
+
+			testTooltip.show();
+
+			tooltipCloseEl.dispatchEvent(e);
+			/* this fails, idk why */
+			//proclaim.isTrue(closeStub.called);
+		});
+
+		it('sets up a close handler for a click on the tooltip-close button', () => {
+			const tooltipEl = document.getElementById('tooltip-demo');
+			const testTooltip = new Tooltip(tooltipEl, { target: 'demo-tooltip-target' });
+			testTooltip.render();
+			const tooltipCloseEl = tooltipEl.querySelector('.o-tooltip-close');
+
+			proclaim.isFalse(closeStub.called);
+
+			tooltipCloseEl.click();
+
+			proclaim.isFalse(closeStub.called);
+
+			testTooltip.show();
+
+			tooltipCloseEl.click();
+			proclaim.isTrue(closeStub.called);
+		});
+
+		it('sets up a viewport resize handler', () => {
+
+			const tooltipEl = document.getElementById('tooltip-demo');
+			const testTooltip = new Tooltip(tooltipEl, { target: 'demo-tooltip-target' });
+
+			testTooltip.render();
+
+			const resizeEvent = new Event('oViewport.resize');
+			document.body.dispatchEvent(resizeEvent);
+
+			proclaim.isFalse(resizeListenerStub.called);
+
+			testTooltip.show();
+
+			document.body.dispatchEvent(resizeEvent);
+
+			proclaim.isTrue(resizeListenerStub.called);
+		});
+
+		it('sets up a key listener to handle esc key', () => {
+			const keyUpListenerSpy = sinon.spy(Tooltip.prototype, 'closeOnKeyUp');
+			const tooltipEl = document.getElementById('tooltip-demo');
+			const testTooltip = new Tooltip(tooltipEl, { target: 'demo-tooltip-target' });
+
+			testTooltip.render();
+
+			const keyEvent = new Event('keyup');
+			document.body.dispatchEvent(keyEvent);
+
+			proclaim.isFalse(keyUpListenerSpy.called);
+
+			testTooltip.show();
+
+			document.body.dispatchEvent(keyEvent);
+
+			proclaim.isTrue(keyUpListenerSpy.called);
+		});
+
+		it('calls drawTooltip', () => {
+			const tooltipEl = document.getElementById('tooltip-demo');
+			const testTooltip = new Tooltip(tooltipEl, { target: 'demo-tooltip-target' });
+
+			testTooltip.show();
+			proclaim.isTrue(drawTooltipStub.called);
+		});
+
+		it('emits oTooltip.show event', function (done) {
+			this.timeout(1000);
+
+			const timer = setTimeout(() => {
+				proclaim.fail('oTooltip.show event to fire', 'oTooltip.show event did not fire');
+			}, 500);
+
+			const tooltipEl = document.getElementById('tooltip-demo');
+			const testTooltip = new Tooltip(tooltipEl, { target: 'demo-tooltip-target' });
+			testTooltip.delegates.tooltip.on('oTooltip.show', () => {
+				clearTimeout(timer);
+				done();
+			});
+
+			testTooltip.show();
+		});
+	});
+
+	describe('drawTooltip', () => {
 		it(' sets tooltip position as requested when in bounds', () => {
 			fixtures.declarativeCode();
 			const evaulateTooltipStub = sandbox.stub(Tooltip.prototype, '_evaulateTooltip');
@@ -325,6 +459,209 @@ describe("Tooltip", () => {
 		});
 		afterEach(() => {
 			fixtures.reset();
+		});
+	});
+
+	describe("#resizeListener", () => {
+
+		let drawTooltipStub;
+		let mockRaf;
+
+		beforeEach(() => {
+			drawTooltipStub = sinon.stub(Tooltip.prototype, 'drawTooltip');
+
+			mockRaf = createMockRaf();
+
+			fixtures.declarativeCode();
+		});
+
+		afterEach(() => {
+			drawTooltipStub.restore();
+			fixtures.reset();
+			window.requestAnimationFrame.restore();
+		});
+
+		it("redraws if triggered and if the tooltip is visible", () => {
+			const testTooltip = Tooltip.init('#tooltip-demo');
+			testTooltip.visible = true;
+			sinon.stub(window, 'requestAnimationFrame').callsFake(mockRaf.raf);
+
+			testTooltip.resizeListener();
+			mockRaf.step({ count: 1 });
+
+			proclaim.isTrue(drawTooltipStub.called);
+		});
+
+		it("does not redraw if the tooltip is not visible", () => {
+			const testTooltip = Tooltip.init('#tooltip-demo');
+			testTooltip.visible = false;
+			sinon.stub(window, 'requestAnimationFrame').callsFake(mockRaf.raf);
+
+			testTooltip.resizeListener();
+			mockRaf.step({ count: 1 });
+
+			proclaim.isFalse(drawTooltipStub.called);
+		});
+
+	});
+
+	describe("#close", () => {
+		let drawTooltipStub;
+
+		beforeEach(() => {
+			drawTooltipStub = sinon.stub(Tooltip.prototype, 'drawTooltip');
+			fixtures.declarativeCode();
+		});
+
+		afterEach(() => {
+			drawTooltipStub.restore();
+			fixtures.reset();
+		});
+
+		it("sets tooltip.visible to false", () => {
+			const testTooltip = Tooltip.init('#tooltip-demo');
+			testTooltip.show();
+			proclaim.isTrue(testTooltip.visible);
+			testTooltip.close();
+			proclaim.isFalse(testTooltip.visible);
+		});
+
+		it.skip("sets display none on the tooltip", function (done) {
+			const testTooltip = Tooltip.init('#tooltip-demo');
+			testTooltip.tooltipEl.style.transition = 'all .1s linear'; // Needed to fire transitionend
+			testTooltip.show();
+			proclaim.notStrictEqual(testTooltip.tooltipEl.style.display, 'none');
+
+			testTooltip.tooltipEl.addEventListener('transitionend', () => {
+				window.setTimeout(() => { // This is a bit race-y for some reason.
+					proclaim.strictEqual(testTooltip.tooltipEl.style.display, 'none');
+					done();
+				}, 0);
+			});
+
+			testTooltip.close();
+		});
+
+		it('emits oTooltip.close event', function (done) {
+			this.timeout(1000);
+
+			const timer = setTimeout(() => {
+				proclaim.fail('oTooltip.close event to fire', 'oTooltip.close event did not fire');
+			}, 500);
+
+			const tooltipEl = document.getElementById('tooltip-demo');
+			const testTooltip = new Tooltip(tooltipEl, { target: 'demo-tooltip-target' });
+			testTooltip.delegates.tooltip.on('oTooltip.close', () => {
+				clearTimeout(timer);
+				done();
+			});
+
+			testTooltip.show();
+			testTooltip.close();
+		});
+
+
+		describe('when called with fireCloseEvent=false', function () {
+			it('skips emitting oTooltip.close event', function (done) {
+				this.timeout(1000);
+
+				const timer = setTimeout(() => {
+					done();
+				}, 500);
+
+				const tooltipEl = document.getElementById('tooltip-demo');
+				const testTooltip = new Tooltip(tooltipEl, { target: 'demo-tooltip-target' });
+				testTooltip.delegates.tooltip.on('oTooltip.close', () => {
+					clearTimeout(timer);
+					proclaim.fail('oTooltip.close event to not fire', 'oTooltip.close event did fire');
+				});
+
+				testTooltip.show();
+				testTooltip.close('', '', false);
+			});
+
+		});
+
+	});
+
+	describe("#closeOnKeyUp", () => {
+		let drawTooltipStub;
+
+		beforeEach(() => {
+			drawTooltipStub = sinon.stub(Tooltip.prototype, 'drawTooltip');
+			fixtures.declarativeCode();
+		});
+
+		afterEach(() => {
+			drawTooltipStub.restore();
+			fixtures.reset();
+		});
+
+		it("only closes of the key was Esc", () => {
+
+			let closeStub = sinon.stub(Tooltip.prototype, 'close');
+
+			const testTooltip = Tooltip.init('#tooltip-demo');
+			testTooltip.show();
+
+			let enterKeypress = document.createEvent('Event');
+			enterKeypress.keyCode = 13;
+
+			testTooltip.closeOnKeyUp(enterKeypress);
+			proclaim.isFalse(closeStub.called);
+
+			let escKeypress = document.createEvent('Event');
+			escKeypress.keyCode = 27;
+			testTooltip.closeOnKeyUp(escKeypress);
+
+			proclaim.isTrue(closeStub.called);
+			closeStub.restore();
+		});
+	});
+
+	describe('throwError', () => {
+		it("thows an error", () => {
+			proclaim.throws(() => { Tooltip.throwError('some message'); });
+		});
+	});
+
+	describe('width', () => {
+		let drawTooltipStub;
+
+		beforeEach(() => {
+			drawTooltipStub = sinon.stub(Tooltip.prototype, 'drawTooltip');
+			fixtures.declarativeCode();
+		});
+
+		afterEach(() => {
+			drawTooltipStub.restore();
+			fixtures.reset();
+		});
+
+		it("returns a number", () => {
+			const testTooltip = Tooltip.init('#tooltip-demo');
+			testTooltip.show();
+			proclaim.isNumber(testTooltip.width());
+		});
+	});
+
+	describe('height', () => {
+		let drawTooltipStub;
+
+		beforeEach(() => {
+			drawTooltipStub = sinon.stub(Tooltip.prototype, 'drawTooltip');
+			fixtures.declarativeCode();
+		});
+
+		afterEach(() => {
+			drawTooltipStub.restore();
+			fixtures.reset();
+		});
+
+		it("returns a number", () => {
+			const testTooltip = Tooltip.init('#tooltip-demo');
+			testTooltip.show();
+			proclaim.isNumber(testTooltip.height());
 		});
 	});
 
